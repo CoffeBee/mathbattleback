29ec2c86fc461268277e4143fd4758ce67b87c45
Ivan Podvorniy <ivan.podvorniy@yandex.ru>
2020-10-07 20:17:30 +0300

    fix ip

--- Sources/App/database.swift
+++ Sources/App/database.swift
@@ -10,7 +10,7 @@ import Vapor
 
 func database_init(_ app: Application) {
     app.databases.use(.postgres(
-        hostname: Environment.get("DATABASE_HOST") ?? "192.168.1.75",
+        hostname: Environment.get("DATABASE_HOST") ?? "127.0.0.1",
         port: 6432,
         username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
         password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
