f26e42a60a7c4717b752362b4eeb655b29bd4951
CoffeBee <ivan.podvorni@yandex.ru>
2020-10-14 13:57:58 +0300

    fix port

--- Sources/App/database.swift
+++ Sources/App/database.swift
@@ -11,7 +11,7 @@ import Vapor
 func database_init(_ app: Application) {
     app.databases.use(.postgres(
         hostname: Environment.get("DATABASE_HOST") ?? "127.0.0.1",
-        port: 6432,
+        port: 5432,
         username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
         password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
         database: Environment.get("DATABASE_NAME") ?? "vapor_database"
